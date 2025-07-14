74dc9dbcdf9e3a83f174d092df42bf6a17755799
senabibi <c2011029@student.cankaya.edu.tr>
2025-07-14 18:58:08 +0200

    Controlled graph

--- tutorials/hist/hist000_TH1_first.py
+++ tutorials/hist/hist000_TH1_first.py
@@ -1,34 +1,32 @@
 ## \file
 ## \ingroup tutorial_hist
-## Modern Python version of Hello World example for histograms
+## Hello World example for TH1
 ##
-## Demonstrates ROOT histogram creation with matplotlib/mplhep visualization
+## Shows how to create, fill and write a histogram to a ROOT file.
 ##
 ## \macro_code
 ##
-## \date JULY 2025
+## \date November 2024
 ## \author Giacomo Parolini (CERN)
-## \author Modernized by Nursena Bitirgen (SUMM)
 
-import ROOT # ROOT data analysis framework
-import mplhep as hp #Matplotlib extension for HEP style plots
-import matplotlib.pyplot as plt #Matplotlib for plotting
+import ROOT
 
-#1.Create and fill the histogram
-hist=ROOT.TH1F("hist","My first histogram",10,0,10)
-my_list=[1,2,3,4,5,6,6,7,8,9,10] #Sample data
-for val in my_list:
-    hist.Fill(val) #Fill the hist 
-#2.Occur ROOT File
+# Open the file to write the histogram to
 with ROOT.TFile.Open("outfile.root", "RECREATE") as outFile:
-    outFile.WriteObject(hist, hist.GetName()) #Save the hist to ROOT file
-    
-#Visulation with matplotlib and mplhep
-plt.style.use(hp.style.ROOT)
-hp.histplot(hist,histtype="fill", label="My first histogram", color="royalblue")
-plt.xlabel("Value") #X-axis label
-plt.ylabel("Entries") #Y-axis label
-plt.legend()
-plt.show()
-plt.savefig("histogram.png") #Save the plot as PNG file
-print("Histogram saved to histogram.png")
+   # Create the histogram object
+   # There are several constructors you can use (see TH1). In this example we use the
+   # simplest one, accepting a number of bins and a range.
+   histogram = ROOT.TH1D("histogram", "My first ROOT histogram", nbinsx = 30, xlow = 0.0, xup = 10.0)
+
+   # Fill the histogram. In this simple example we use a fake set of data.
+   # The 'D' in TH1D stands for 'double', so we fill the histogram with doubles.
+   # In general you should prefer TH1D over TH1F unless you have a very specific reason
+   # to do otherwise.
+   values = [1, 2, 3, 3, 3, 4, 3, 2, 1, 0]
+   for val in values:
+      histogram.Fill(val)
+
+   # Write the histogram to `outFile`.
+   outFile.WriteObject(histogram, histogram.GetName())
+
+   # When `with` block exits, `outFile` will close itself and write its contents to disk.
