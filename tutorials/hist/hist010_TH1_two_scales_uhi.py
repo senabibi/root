a26df7941f5a401122567cc5487cc7d360ca4928
senabibi <c2011029@student.cankaya.edu.tr>
2025-07-17 17:21:19 +0200

    changed the order and fixed

--- tutorials/hist/hist010_TH1_two_scales_uhi.py
+++ tutorials/hist/hist010_TH1_two_scales_uhi.py
@@ -6,13 +6,10 @@ import mplhep as hep
 np.random.seed(0)
 plt.style.use(hep.style.ROOT)
 h1 = ROOT.TH1F("h1", "my histogram", 100, -3, 3)
-hint1 = ROOT.TH1F("hint1", "h1 bins integral", 100, -3, 3)
 counts,_= np.histogram(np.random.normal(0, 1, 10000), range=(-3,3), bins=100)
-h1[...]=counts  # Fill h1 with the generated random numbers
-
-# Numpy ile kümülatif toplamı hesapla ve hint1'e ata
+h1[...]=counts  
+hint1 = ROOT.TH1F("hint1", "h1 bins integral", 100, -3, 3)
 hint1[...] = np.cumsum(h1.values())
-
 fig, ax1 = plt.subplots(figsize=(10, 6))
 hep.histplot(h1,ax=ax1,histtype='fill',color='white',edgecolor='blue',linewidth=1.5,alpha=0.5)
 ax2 = ax1.twinx()
