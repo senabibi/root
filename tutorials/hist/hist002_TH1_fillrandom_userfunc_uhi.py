420bf21ed802840343959d8f16e6b9cc83ea762f
senabibi <c2011029@student.cankaya.edu.tr>
2025-07-29 10:56:07 +0200

    Add UHI Python example

--- tutorials/hist/hist002_TH1_fillrandom_userfunc_uhi.py
+++ tutorials/hist/hist002_TH1_fillrandom_userfunc_uhi.py
@@ -4,22 +4,17 @@ import numpy as np
 form1 = ROOT.TFormula("form1", "abs(sin(x)/x)")
 rangeMin = 0.0
 rangeMax = 10.0
-sqroot = ROOT.TF1("sqroot", "x*gaus(0) + [3]*form1", 0.0, 10.0)
+sqroot = ROOT.TF1("sqroot", "x*gaus(0) + [3]*form1", rangeMin, rangeMax)
 gausScale = 10.0  # [0]
 gausMean = 4.0    # [1]
 gausVar = 1.0     # [2]
 form1Scale = 20.0 # [3]
 sqroot.SetParameters(gausScale, gausMean, gausVar, form1Scale)
 h1d = ROOT.TH1D("h1d", "Test random numbers", 200, rangeMin, rangeMax)
-# Use our user-defined function to fill the histogram with random values sampled from it.
 # Simple approximation using just uniform random numbers
-random_numbers = np.array([sqroot.GetRandom() for _ in range(10000)])
-counts,_=np.histogram(random_numbers, bins=200, range=(rangeMin, rangeMax))
-h1d[...]=counts
-
+h1d[...]=np.histogram(np.array([sqroot.GetRandom() for _ in range(10000)]), bins=200, range=(rangeMin, rangeMax))
 # Open a ROOT file and save the formula, function and histogram
 with ROOT.TFile.Open("fillrandom_userfunc_py.root", "RECREATE") as myFile:
    myFile.WriteObject(form1, form1.GetName())
    myFile.WriteObject(sqroot, sqroot.GetName())
    myFile.WriteObject(h1d, h1d.GetName())
-  
