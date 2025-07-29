c7a63497ea8832bfbcdf4bc79414559195e4375e
senabibi <c2011029@student.cankaya.edu.tr>
2025-07-29 11:06:58 +0200

    Added UHI Python Example

--- tutorials/hist/hist030_TRatioPlot_residual.uhi.py
+++ tutorials/hist/hist030_TRatioPlot_residual.uhi.py
@@ -1,21 +1,14 @@
 import ROOT
 import numpy as np
 ROOT.gStyle.SetOptStat(0)
-
 c1 = ROOT.TCanvas("c1", "fit residual simple")
 h1 = ROOT.TH1D("h1", "h1", 50, -5, 5)
-
 h1.FillRandom("gaus", 2000)
-random_numbers = np.random.normal(0.0, 1.0, 2000)
-counts, _ = np.histogram(random_numbers, bins=50, range=(-5, 5))
-h1[:]=counts
+h1[:]=np.histogram(np.random.normal(0.0, 1.0, 2000), bins=50, range=(-5, 5))[0]
 h1.Fit("gaus")
 h1.GetXaxis().SetTitle("x")
-
 rp1 = ROOT.TRatioPlot(h1)
 rp1.Draw()
 rp1.GetLowerRefYaxis().SetTitle("ratio")
 rp1.GetUpperRefYaxis().SetTitle("entries")
-
 c1.Update()
-input("Press Enter to continue...")
