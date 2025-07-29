668237a21d8a556279a36b8346d0812dfe79f26b
senabibi <c2011029@student.cankaya.edu.tr>
2025-07-29 10:52:38 +0200

    Add UHI Python example

--- tutorials/hist/hist001_TH1_fillrandom_uhi.py
+++ tutorials/hist/hist001_TH1_fillrandom_uhi.py
@@ -2,10 +2,7 @@ import ROOT
 import numpy as np    
 # Create a histogram using ROOT
 h = ROOT.TH1D("h1d", "Test random numbers", 200, 0.0, 10.0) 
-# mean = 0.0, standard deviation = 1.0
-random_numbers = np.random.normal(0.0, 1.0, 10000)
 # Create a numpy histogram with the same binning
-counts,_=np.histogram(random_numbers, bins=200, range=(0.0, 10.0))
-h[:]=counts
+h[:]=np.histogram(np.random.normal(0.0, 1.0, 10000), bins=200, range=(0.0, 10.0))
 with ROOT.TFile.Open("fillrandom_py.root", "RECREATE") as myfile:
     myfile.WriteObject(h, h.GetName())
